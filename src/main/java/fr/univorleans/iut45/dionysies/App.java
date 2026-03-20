58f77ada9869731721b4bdf090d5dbaf4f46ba00
Sophocle <contact@drama.org>
2026-03-20 14:01:41 +0100

    on démarre avec maven

---
+++ src/main/java/fr/univorleans/iut45/dionysies/App.java
@@ -0,0 +1,13 @@
+package fr.univorleans.iut45.dionysies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fr/univorleans/iut45/dionysies/AppTest.java
@@ -0,0 +1,20 @@
+package fr.univorleans.iut45.dionysies;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
